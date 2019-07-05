878ffe60dfa1ae6f5b143ef48a5f1694e4a190b9
CuriousEngineer <kirill.s.works@gmail.com>
2019-07-05 16:46:53 +0300

    Added some methods in Shortener.

--- src/com/pack/Shortener.java
+++ src/com/pack/Shortener.java
@@ -1,10 +1,25 @@
 package com.pack;
 
+import com.pack.strategy.StorageStrategy;
+
 public class Shortener {
+    private Long lastId = 0l;  // отвечает за последнее значение идентификатора
+    private StorageStrategy storageStrategy; // хранит стратегию
 
-    public Long getId(String string){return null;}
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
+    }
 
-    public String getString(Long id){return null;}
+    public synchronized Long getId(String string){
+        if (storageStrategy.containsValue(string)) return storageStrategy.getKey(string);
+        else {
+            lastId++;
+            storageStrategy.put(lastId, string);
+            return lastId;
+        }
+    }
 
-
+    public synchronized String getString(Long id){
+        return storageStrategy.getValue(id);
+    }
 }
